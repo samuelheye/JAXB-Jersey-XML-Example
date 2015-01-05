f7858afc20923a59aa106319773ee822c8758553
sambagurd <sambagurd@gmail.com>
2015-01-04 18:35:34 -0600

    First Commit

---
+++ src/com/one/XMLService.java
@@ -0,0 +1,25 @@
+package com.one;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/xml/customer")
+public class XMLService {
+ 
+	@GET
+	@Path("/{pin}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Customer getCustomerInXML(@PathParam("pin") int pin) {
+ 
+		Customer customer = new Customer();
+		customer.setName("Samuel");
+		customer.setPin(pin);
+ 
+		return customer;
+ 
+	}
+ 
+}
